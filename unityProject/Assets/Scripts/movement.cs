aa577620bd04c1e3340e7b6569da43bf41180727
Konrad <malyduo@gmail.com>
2016-04-07 22:55:45 +0200

    Nowa wersja

---
+++ unityProject/Assets/Scripts/movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+	public Vector3 teleportPoint;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate(Vector3.forward * Time.deltaTime);
+	}
+
+	void FixedUpdate() {
+		rb.MovePosition(transform.position + transform.forward * Time.deltaTime);
+	}
+}
